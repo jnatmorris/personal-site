f4911cdd4bcae12118c630ed870eb3dce1754ff9
Justin Morris <jnatmorris@gmail.com>
2022-05-06 22:19:18 +0200

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,6 +21,20 @@ const Home: NextPage = () => {
         [0, 200]
     );
 
+    const Variants: Variants = {
+        hidden: { opacity: 0 },
+        show: {
+            opacity: 1,
+            transition: {
+                staggerChildren: 1,
+            },
+        },
+    };
+
+    const item: Variants = {
+        hidden: { opacity: 0 },
+        show: { opacity: 1 },
+    };
     return (
         <>
             <Head>
@@ -31,7 +45,28 @@ const Home: NextPage = () => {
 
             <div className="relative h-screen">
                 <m.div className="mt-[20vh]" style={{ y: yHeader }}>
-                    <Howdy />
+                    <m.div
+                        variants={Variants}
+                        animate={"show"}
+                        initial={"hidden"}
+                        className="ml-[5vw] prose-h1:m-0 prose-h2:m-0 prose-h3:m-0"
+                    >
+                        <m.div variants={item} className="flex pb-3 space-x-2">
+                            <h1 className="underline decoration-blue-400 underline-offset-2">
+                                Howdy,
+                            </h1>
+                            <h1>&#x1F44B;</h1>
+                        </m.div>
+
+                        <m.h2 variants={item}>
+                            I&apos;m Justin Morris. A...
+                        </m.h2>
+                        <m.h3 variants={item}>- Computer science student</m.h3>
+
+                        <m.h3 variants={item}>- Web developer</m.h3>
+
+                        <m.h3 variants={item}>- Curiosity driven</m.h3>
+                    </m.div>
                 </m.div>
             </div>
 
