61a4090ace584b7c12d5141b91be337cae78541f
Justin Morris <jnatmorris@gmail.com>
2022-05-12 15:34:54 +0200

    cleanup connect

--- Components/Connect.tsx
+++ Components/Connect.tsx
@@ -1,46 +1,41 @@
 import * as React from "react";
-import { m } from "framer-motion";
 
 const Connect: React.FC = () => (
     <div className="mx-[2vw] pb-[2.5vh] opacity-100 lg:mx-[0vw]">
-        <h3 className="pb-[1vh] text-2xl underline decoration-sky-600 underline-offset-2 lg:text-4xl">
-            get to know me
-        </h3>
-
-        <div className="mx-[0.5vw] mt-1 flex justify-between prose-h3:m-0">
-            <m.h4 whileTap={{ scale: 0.9 }}>
+        <div className="mx-[0.5vw] mt-1 flex justify-around border-t-2 border-gray-300 pt-2 prose-h3:m-0">
+            <h4>
                 <a
-                    className="text-xl no-underline hover:underline lg:text-2xl"
+                    className="text-xl no-underline lg:text-3xl"
                     href="https://github.com/JNat07"
                 >
                     GitHub
                 </a>
-            </m.h4>
-            <m.h4 whileTap={{ scale: 0.9 }}>
+            </h4>
+            <h4>
                 <a
-                    className="text-xl no-underline hover:underline lg:text-2xl"
+                    className="text-xl no-underline  lg:text-3xl"
                     href="https://www.linkedin.com/in/justinnmorris/"
                 >
                     LinkedIn
                 </a>
-            </m.h4>
-            <m.h4 whileTap={{ scale: 0.9 }}>
+            </h4>
+            <h4>
                 <a
-                    className="text-xl no-underline hover:underline lg:text-2xl"
+                    className="text-xl no-underline  lg:text-3xl"
                     href="mailto:justin@jnmorris.dev"
                 >
                     Email
                 </a>
-            </m.h4>
-            <m.h4 whileTap={{ scale: 0.9 }} className="relative">
+            </h4>
+            <h4>
                 <a
-                    className="text-xl no-underline hover:underline lg:text-2xl"
+                    className="text-xl no-underline  lg:text-3xl"
                     href="./CV/CV.pdf"
                     download={"JustinMorrisResume"}
                 >
                     Resume
                 </a>
-            </m.h4>
+            </h4>
         </div>
     </div>
 );
