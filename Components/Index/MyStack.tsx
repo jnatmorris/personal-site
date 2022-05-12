d9952af576df28eb0f43404a5696db84e741d983
Justin Morris <jnatmorris@gmail.com>
2022-05-12 15:35:10 +0200

    change sizing

--- Components/Index/MyStack.tsx
+++ Components/Index/MyStack.tsx
@@ -2,15 +2,13 @@ import * as React from "react";
 
 const MyStack: React.FC = () => (
     <>
-        <h3 className="pb-[1vh] text-2xl  underline decoration-sky-600 underline-offset-2 lg:text-4xl">
-            my stack
-        </h3>
+        <h3 className="text-2xl lg:text-4xl">My stack</h3>
         <h4 className="text-xl font-normal leading-relaxed lg:text-2xl lg:leading-loose">
             While I have my preferences, based off experiences, I&apos;m always
             flexible to trying something new. Here is what I&apos;m currently
             using:
         </h4>
-        <ul className="text-xl list-outside lg:text-2xl ">
+        <ul className="list-outside text-xl lg:text-2xl ">
             <li>
                 <h5>
                     ReactJS -{" "}
