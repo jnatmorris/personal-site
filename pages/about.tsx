7eeee4da98afb49435f8310ca0c502e13ef1bd60
Justin Morris <jnatmorris@gmail.com>
2022-05-14 15:54:51 +0200

    cleanup

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,7 +3,6 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Doodler from "../Components/About/Doodles";
 import Pilot from "../Components/About/Pilot";
-import About from "../Components/About/AboutHeader";
 
 const AboutPage: NextPage = () => {
     return (
@@ -11,31 +10,9 @@ const AboutPage: NextPage = () => {
             <Head>
                 <title>Justin Morris</title>
                 <meta name="description" content="About" />
-                <link
-                    rel="apple-touch-icon"
-                    sizes="180x180"
-                    href="/apple-touch-icon.png"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="32x32"
-                    href="/favicon-32x32.png"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="16x16"
-                    href="/favicon-16x16.png"
-                />
-                <link rel="manifest" href="/site.webmanifest" />
             </Head>
             <main className="overflow-hidden">
-                <div className="h-screen">
-                    <About />
-                </div>
-
-                <div className="space-y-[15vh] lg:gap-y-[1vh] lg:space-y-0">
+                <div className=" space-y-[15vh] lg:gap-y-[1vh] lg:space-y-0">
                     <div className="space-y-[15vh] lg:grid lg:grid-cols-2 lg:space-y-[0vh] ">
                         <div className="lg:border-r-[1.5px] lg:border-gray-200 lg:pr-[1vw] lg:opacity-50 lg:hover:opacity-100 dark:lg:border-slate-800">
                             <Doodler />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
-import Welcome from "../Components/Index/Welcome";
+
 import MyMentality from "../Components/Index/MyMentality";
 import MyStack from "../Components/Index/MyStack";
 import OnMyRadar from "../Components/Index/OnMyRadar";
+import AboutMe from "../Components/Index/AboutMe";
 
 const Home: NextPage = () => {
     return (
@@ -13,38 +14,19 @@ const Home: NextPage = () => {
                 <title>Justin Morris</title>
                 <meta name="description" content="Justin's Personal Site" />
                 <link rel="icon" href="/favicon.ico" />
-
-                <link
-                    rel="apple-touch-icon"
-                    sizes="180x180"
-                    href="/apple-touch-icon.png"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="32x32"
-                    href="/favicon-32x32.png"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="16x16"
-                    href="/favicon-16x16.png"
-                />
-                <link rel="manifest" href="/site.webmanifest" />
             </Head>
 
             <main className="overflow-hidden">
                 <div className="h-screen">
-                    <Welcome />
+                    <AboutMe />
                 </div>
 
                 <div className="space-y-[15vh]  lg:space-y-[1vh]">
                     <div className="space-y-[15vh] lg:grid lg:grid-cols-2 lg:gap-y-[4vh] lg:space-y-[0vh]">
-                        <div className="place-self-center lg:border-r-2 lg:border-gray-200 lg:pr-[1vw] lg:opacity-50 lg:hover:opacity-100">
+                        <div className="place-self-center lg:opacity-50 lg:hover:opacity-100">
                             <MyStack />
                         </div>
-                        <div className="place-self-center lg:border-l-2 lg:border-gray-200 lg:pl-[1vw]  lg:opacity-50 lg:hover:opacity-100">
+                        <div className="place-self-center lg:opacity-50 lg:hover:opacity-100">
                             <OnMyRadar />
                         </div>
                         <div className="w-full lg:col-span-2 lg:h-0.5 lg:bg-gray-200" />
