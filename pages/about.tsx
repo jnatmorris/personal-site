8f1ffcff268bec2986195a7eb4e9a89df4caceb5
Justin Morris <jnatmorris@gmail.com>
2022-05-22 14:37:38 +0200

    update pages

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,8 +1,10 @@
 import * as React from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import Doodler from "../Components/About/Doodles";
 import Pilot from "../Components/About/Pilot";
+import Navigation from "../Components/Navigation";
 
 const AboutPage: NextPage = () => {
     return (
@@ -11,19 +13,26 @@ const AboutPage: NextPage = () => {
                 <title>Justin Morris</title>
                 <meta name="description" content="About" />
             </Head>
-            <main className="overflow-hidden">
-                <div className=" space-y-[15vh] lg:gap-y-[1vh] lg:space-y-0">
-                    <div className="space-y-[15vh] lg:grid lg:grid-cols-2 lg:space-y-[0vh] ">
-                        <div className="lg:border-r-[1.5px] lg:border-gray-200 lg:pr-[1vw] lg:opacity-50 lg:hover:opacity-100 dark:lg:border-slate-800">
-                            <Doodler />
-                        </div>
 
-                        <div className="lg:border-l-[1.5px] lg:border-gray-200 lg:pl-[1.5vw] lg:opacity-50 lg:hover:opacity-100 dark:lg:border-slate-800">
-                            <Pilot />
+            <p>Under construction</p>
+            {/* <Parallax pages={1}>
+                <ParallaxLayer offset={0} sticky={{ start: 0, end: 0.3 }}>
+                    <Navigation />
+                </ParallaxLayer>
+                <ParallaxLayer>
+                    <div className=" space-y-[15vh] lg:gap-y-[1vh] lg:space-y-0">
+                        <div className="space-y-[15vh] lg:grid lg:grid-cols-2 lg:space-y-[0vh] ">
+                            <div className="lg:border-r-[1.5px] lg:border-gray-200 lg:pr-[1vw] lg:opacity-50 lg:hover:opacity-100 dark:lg:border-slate-800">
+                                <Doodler />
+                            </div>
+
+                            <div className="lg:border-l-[1.5px] lg:border-gray-200 lg:pl-[1.5vw] lg:opacity-50 lg:hover:opacity-100 dark:lg:border-slate-800">
+                                <Pilot />
+                            </div>
                         </div>
                     </div>
-                </div>
-            </main>
+                </ParallaxLayer>
+            </Parallax> */}
         </>
     );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,23 @@
 import * as React from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
-import MyMentality from "../Components/Index/MyMentality";
-import MyStack from "../Components/Index/MyStack";
-
+import Navigation from "../Components/Navigation";
 import AboutMe from "../Components/Index/AboutMe";
+
+// Chatapp
+import ChatApp from "../Components/Index/myWork/ChatApp/ChatApp";
+import {
+    ChatAppImgDark,
+    ChatAppImgLight,
+} from "../Components/Index/myWork/ChatApp/ChaAppImgs";
+// Flag game
+import FlagGame from "../Components/Index/myWork/FlagGame/FlagGame";
+import {
+    FlagGameGame,
+    FlagGameLobby,
+} from "../Components/Index/myWork/FlagGame/FlagGameImg";
 
 const Home: NextPage = () => {
     return (
@@ -15,18 +27,112 @@ const Home: NextPage = () => {
                 <meta name="description" content="Justin's Personal Site" />
             </Head>
 
-            <div className="h-screen">
-                <div className="pt-[15vh] lg:pt-[25vh]">
-                    <AboutMe />
-                </div>
-            </div>
+            <Parallax pages={5} className="bg-white dark:bg-black">
+                <ParallaxLayer offset={0} sticky={{ start: 0, end: 0.3 }}>
+                    <Navigation />
+                </ParallaxLayer>
+                <ParallaxLayer
+                    offset={0.2}
+                    speed={0}
+                    className="bg-white dark:bg-black"
+                >
+                    <div className="mx-[10vw] lg:mx-[5vw]">
+                        <AboutMe />
+                    </div>
+                </ParallaxLayer>
+                <ParallaxLayer
+                    offset={1}
+                    speed={2}
+                    sticky={{ start: 1, end: 1.7 }}
+                    className="grid grid-cols-2"
+                >
+                    <div className="col-span-1 ml-[5vw] place-items-center lg:pt-[30vh] ">
+                        <ChatApp />
+                    </div>
+                </ParallaxLayer>
+                {/* ChatApp Login Images */}
+                <ParallaxLayer offset={1.5} speed={1}>
+                    <div className="mx-[10vw] grid grid-cols-2">
+                        <div className="col-span-1" />
+                        <ChatAppImgDark />
+                    </div>
+                </ParallaxLayer>
 
-            <div className="mb-[4vh] h-0.5 rounded-lg bg-slate-300" />
-            <div className="space-y-[15vh] lg:grid lg:grid-cols-2 lg:gap-[1vw] lg:space-y-[0vh] ">
-                <MyMentality />
+                <ParallaxLayer offset={1.8} speed={1.5}>
+                    <div className="mx-[10vw] grid grid-cols-2">
+                        <div className="col-span-1" />
+                        <ChatAppImgLight />
+                    </div>
+                </ParallaxLayer>
 
-                <MyStack />
-            </div>
+                {/* ChatApp Chat Images */}
+                <ParallaxLayer
+                    offset={4}
+                    speed={1.5}
+                    sticky={{ start: 3, end: 6 }}
+                    className="z-50 grid grid-cols-2"
+                >
+                    <div className="col-span-1 ml-[5vw] place-items-center lg:pt-[30vh] ">
+                        <FlagGame />
+                    </div>
+                </ParallaxLayer>
+
+                <ParallaxLayer offset={3.9} speed={1}>
+                    <div className="mx-[10vw] grid grid-cols-2">
+                        <div className="col-span-1" />
+                        <FlagGameLobby />
+                    </div>
+                </ParallaxLayer>
+
+                <ParallaxLayer offset={3.5} speed={2}>
+                    <div className="mx-[10vw] grid grid-cols-2">
+                        <div className="col-span-1" />
+                        <FlagGameGame />
+                    </div>
+                </ParallaxLayer>
+
+                {/* <ParallaxLayer
+                    offset={4}
+                    speed={1}
+                    sticky={{ start: 4, end: 5 }}
+                    className="bg-white dark:bg-black"
+                >
+                    <div className="lg:w-1/2">
+                        <UniSystem />
+                    </div>
+                </ParallaxLayer>
+
+                <ParallaxLayer
+                    offset={4}
+                    speed={1.5}
+                    className="bg-white dark:bg-black"
+                >
+                    <div className="absolute right-0 w-1/2">
+                        <div className="flex justify-center">
+                            <div className="relative w-[45%]">
+                                <Image
+                                    layout="responsive"
+                                    src={UniSystemImage}
+                                    width={696}
+                                    height={1510}
+                                    quality={100}
+                                    className="rounded-md"
+                                    placeholder="blur"
+                                    alt="UniSystem"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </ParallaxLayer> */}
+                {/* <ParallaxLayer offset={2} speed={0.5}>
+                    <div className="bg-blue-300 lg:w-1/2">
+                        <MyStack />
+                    </div>
+                </ParallaxLayer> */}
+                {/* <ParallaxLayer offset={1} speed={2}>
+                    <div className="absolute right-0 w-1/2 bg-red-300">d</div>
+                </ParallaxLayer> */}
+            </Parallax>
         </>
     );
 };
