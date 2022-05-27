d6015b80f6cb581de2c775f4053f02014a1190b1
Justin Morris <jnatmorris@gmail.com>
2022-05-27 20:47:14 +0200

    update css

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,6 +19,8 @@ import {
     FlagGameLobby,
 } from "../Components/Index/myWork/FlagGame/FlagGameImg";
 
+import { DeviceWidth } from "../Components/Layout";
+
 const Home: NextPage = () => {
     return (
         <>
@@ -27,112 +29,101 @@ const Home: NextPage = () => {
                 <meta name="description" content="Justin's Personal Site" />
             </Head>
 
-            <Parallax pages={5} className="bg-white dark:bg-black">
-                <ParallaxLayer offset={0.2} sticky={{ start: 0, end: 0.3 }}>
-                    <Navigation />
-                </ParallaxLayer>
-                <ParallaxLayer
-                    offset={0.2}
-                    speed={0}
-                    className="bg-white dark:bg-black"
-                >
-                    <div className="mx-[10vw] lg:mx-[5vw]">
-                        <AboutMe />
-                    </div>
-                </ParallaxLayer>
-                <ParallaxLayer
-                    offset={1}
-                    speed={2}
-                    sticky={{ start: 1, end: 1.7 }}
-                    className="grid grid-cols-2"
-                >
-                    <div className="col-span-1 ml-[5vw] place-items-center lg:pt-[30vh] ">
-                        <ChatApp />
-                    </div>
-                </ParallaxLayer>
-                {/* ChatApp Login Images */}
-                <ParallaxLayer offset={1.5} speed={1}>
-                    <div className="mx-[10vw] grid grid-cols-2">
-                        <div className="col-span-1" />
-                        <ChatAppImgDark />
-                    </div>
-                </ParallaxLayer>
-
-                <ParallaxLayer offset={1.8} speed={1.5}>
-                    <div className="mx-[10vw] grid grid-cols-2">
-                        <div className="col-span-1" />
-                        <ChatAppImgLight />
-                    </div>
-                </ParallaxLayer>
-
-                {/* ChatApp Chat Images */}
-                <ParallaxLayer
-                    offset={4}
-                    speed={1.5}
-                    sticky={{ start: 3, end: 6 }}
-                    className="z-50 grid grid-cols-2"
-                >
-                    <div className="col-span-1 ml-[5vw] place-items-center lg:pt-[30vh] ">
-                        <FlagGame />
-                    </div>
-                </ParallaxLayer>
-
-                <ParallaxLayer offset={3.9} speed={1}>
-                    <div className="mx-[10vw] grid grid-cols-2">
-                        <div className="col-span-1" />
-                        <FlagGameLobby />
-                    </div>
-                </ParallaxLayer>
-
-                <ParallaxLayer offset={3.5} speed={2}>
-                    <div className="mx-[10vw] grid grid-cols-2">
-                        <div className="col-span-1" />
-                        <FlagGameGame />
-                    </div>
-                </ParallaxLayer>
-
-                {/* <ParallaxLayer
-                    offset={4}
-                    speed={1}
-                    sticky={{ start: 4, end: 5 }}
-                    className="bg-white dark:bg-black"
-                >
-                    <div className="lg:w-1/2">
-                        <UniSystem />
-                    </div>
-                </ParallaxLayer>
-
-                <ParallaxLayer
-                    offset={4}
-                    speed={1.5}
-                    className="bg-white dark:bg-black"
-                >
-                    <div className="absolute right-0 w-1/2">
-                        <div className="flex justify-center">
-                            <div className="relative w-[45%]">
-                                <Image
-                                    layout="responsive"
-                                    src={UniSystemImage}
-                                    width={696}
-                                    height={1510}
-                                    quality={100}
-                                    className="rounded-md"
-                                    placeholder="blur"
-                                    alt="UniSystem"
-                                />
+            <DeviceWidth.Consumer>
+                {(width) => (
+                    <Parallax
+                        pages={width > 640 ? 5 : 6}
+                        className="bg-white dark:bg-black lg:block"
+                    >
+                        <ParallaxLayer
+                            offset={0}
+                            sticky={{
+                                start: 0,
+                                end: width > 640 ? 0.3 : 0.15,
+                            }}
+                        >
+                            <Navigation />
+                        </ParallaxLayer>
+                        <ParallaxLayer
+                            offset={0.2}
+                            speed={0}
+                            className="bg-white dark:bg-black"
+                        >
+                            <div className="mx-[10vw] lg:mx-[5vw]">
+                                <AboutMe />
                             </div>
-                        </div>
-                    </div>
-                </ParallaxLayer> */}
-                {/* <ParallaxLayer offset={2} speed={0.5}>
-                    <div className="bg-blue-300 lg:w-1/2">
-                        <MyStack />
-                    </div>
-                </ParallaxLayer> */}
-                {/* <ParallaxLayer offset={1} speed={2}>
-                    <div className="absolute right-0 w-1/2 bg-red-300">d</div>
-                </ParallaxLayer> */}
-            </Parallax>
+                        </ParallaxLayer>
+
+                        <ParallaxLayer
+                            offset={width > 640 ? 1 : 1.2}
+                            speed={width > 640 ? 2 : 1}
+                            sticky={
+                                width > 640 ? { start: 1, end: 1.7 } : undefined
+                            }
+                            className="lg:grid lg:grid-cols-2"
+                        >
+                            <div className="ml-[5vw] lg:col-span-1  lg:place-items-center lg:pt-[30vh]">
+                                <ChatApp />
+                            </div>
+                        </ParallaxLayer>
+
+                        {/* ChatApp Login Images */}
+                        <ParallaxLayer
+                            offset={width > 640 ? 1.5 : 2.1}
+                            speed={width > 640 ? 1 : 0}
+                        >
+                            <div className="mx-[10vw] lg:grid lg:grid-cols-2">
+                                <div className="lg:col-span-1" />
+                                <ChatAppImgDark />
+                            </div>
+                        </ParallaxLayer>
+
+                        <ParallaxLayer
+                            offset={width > 640 ? 1.8 : 2.2}
+                            speed={width > 640 ? 1.5 : 0}
+                        >
+                            <div className="mx-[10vw] lg:grid lg:grid-cols-2">
+                                <div className="lg:col-span-1" />
+                                <ChatAppImgLight />
+                            </div>
+                        </ParallaxLayer>
+
+                        {/* ChatApp Chat Images */}
+                        <ParallaxLayer
+                            offset={width > 640 ? 4 : 3.4}
+                            speed={1}
+                            sticky={
+                                width > 640 ? { start: 3, end: 6 } : undefined
+                            }
+                            className="lg:grid lg:grid-cols-2"
+                        >
+                            <div className="mx-[10vw] ml-[5vw] lg:col-span-1 lg:place-items-center lg:pt-[30vh] ">
+                                <FlagGame />
+                            </div>
+                        </ParallaxLayer>
+
+                        <ParallaxLayer
+                            offset={width > 640 ? 3.9 : 4.2}
+                            speed={width > 640 ? 1 : 0}
+                        >
+                            <div className="mx-[10vw] lg:grid lg:grid-cols-2">
+                                <div className="lg:col-span-1" />
+                                <FlagGameLobby />
+                            </div>
+                        </ParallaxLayer>
+
+                        <ParallaxLayer
+                            offset={width > 640 ? 3.5 : 4.4}
+                            speed={width > 640 ? 2 : 0}
+                        >
+                            <div className="mx-[10vw] lg:grid lg:grid-cols-2">
+                                <div className="lg:col-span-1" />
+                                <FlagGameGame />
+                            </div>
+                        </ParallaxLayer>
+                    </Parallax>
+                )}
+            </DeviceWidth.Consumer>
         </>
     );
 };
