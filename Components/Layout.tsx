06371b5163533e64412f381c3ac361e4b1badb31
Justin Morris <jnatmorris@gmail.com>
2022-05-27 20:47:35 +0200

    move width to layout

--- Components/Layout.tsx
+++ Components/Layout.tsx
@@ -1,6 +1,4 @@
 import * as React from "react";
-import Header from "./Navigation";
-import Connect from "./Connect";
 import Head from "next/head";
 import { LazyMotion, MotionConfig, domAnimation } from "framer-motion";
 
@@ -8,7 +6,18 @@ interface Props {
     children: React.ReactNode;
 }
 
+export const DeviceWidth = React.createContext<number>(0);
+
 const Layout: React.FC<Props> = ({ children }) => {
+    // set width for media queries
+    const [width, setWidth] = React.useState<number>(0);
+    React.useEffect(() => {
+        if (typeof window === "undefined") {
+            return;
+        }
+        setWidth(window.innerWidth);
+    }, []);
+
     return (
         <LazyMotion strict features={domAnimation}>
             <MotionConfig reducedMotion="user">
@@ -34,7 +43,9 @@ const Layout: React.FC<Props> = ({ children }) => {
                 </Head>
 
                 <div className="prose prose-h1:m-0 prose-h2:m-0 prose-h3:m-0 prose-h4:m-0 prose-p:m-0 prose-img:m-0 dark:prose-invert">
-                    <main>{children}</main>
+                    <DeviceWidth.Provider value={width}>
+                        <main>{children}</main>
+                    </DeviceWidth.Provider>
                 </div>
             </MotionConfig>
         </LazyMotion>
