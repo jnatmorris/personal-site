f1b9ad50f9a4da54241ac3b3b1a3e357614c4475
Justin Morris <jnatmorris@gmail.com>
2022-05-28 13:56:39 +0200

    add functionality

--- Components/Typewriter.tsx
+++ Components/Typewriter.tsx
@@ -10,16 +10,22 @@ interface Props {
         font?: string;
         // access to div's class (for tailwindcss)
         outerDivClass?: string;
-        // number of seconds between letters printed on screen
-        delayWrite: number;
+        // number of seconds between letters printed on screen (seconds)
+        timeBetweenLetters?: number;
+        // time before writing
+        delayWrite?: number;
         // visible or invisible cursor
         showCursor?: boolean;
+        // remove cursor when on completed typing
+        removeCursorOnComplete?: boolean;
         // color of cursor
         cursorColor?: string;
         // show cursor post write (only visible if showCursor is true)
         infiniteCCursor?: boolean;
         // callback run when whole string is visible on string
         onComplete?(): void;
+        // when user clicks
+        onClick?(): void;
     };
 }
 
@@ -28,32 +34,40 @@ const TypeWriter: React.FC<Props> = ({ string, options }) => {
         stringClass,
         font,
         outerDivClass,
-        delayWrite,
         showCursor,
-        cursorColor,
-        infiniteCCursor,
+        cursorColor = false,
+        removeCursorOnComplete = false,
+        infiniteCCursor = false,
+        timeBetweenLetters = 2,
         onComplete = () => null,
+        onClick = () => null,
     } = options;
 
     const [time, setTime] = React.useState<number>(0);
+    const [finishedTyping, setFinishedTyping] = React.useState<boolean>(false);
 
     React.useEffect(() => {
         const timeCountId = setInterval(() => {
             if (time >= string.length) {
                 clearInterval(timeCountId);
 
+                // run callback
                 onComplete();
+
+                // if user chooses to remove cursor, then set state to true and remove
+                removeCursorOnComplete ? setFinishedTyping(true) : null;
             } else {
                 setTime((prev) => prev + 1);
             }
-        }, delayWrite * 100);
+        }, timeBetweenLetters * 1000);
+
         return () => {
             clearInterval(timeCountId);
         };
     }, [time]);
 
     return (
-        <div className={"flex" + " " + outerDivClass}>
+        <div className={"flex" + " " + outerDivClass} onClick={() => onClick()}>
             <h1
                 style={font ? { fontFamily: font } : {}}
                 className={stringClass}
@@ -61,7 +75,7 @@ const TypeWriter: React.FC<Props> = ({ string, options }) => {
                 {string.slice(0, time)}
             </h1>
 
-            {showCursor && (
+            {showCursor && !finishedTyping && (
                 <m.h1
                     className={"w-2" + " " + cursorColor}
                     animate={{ opacity: [0, 1, 0] }}
