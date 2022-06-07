ae8c5c6985c806a204e975c1a89d703e6dd23dc8
Justin Morris <jnatmorris@gmail.com>
2022-06-07 12:38:18 -0400

    cleanup and style

--- Components/Index/myWork/FlagGame/FlagGame.tsx
+++ Components/Index/myWork/FlagGame/FlagGame.tsx
@@ -1,57 +1,61 @@
 import * as React from "react";
+import { Typewriter } from "react-simple-typewriter";
 
 const FlagGame: React.FC = () => (
-    <div className="col-span-2">
-        <div className="space-y-[3vh]  lg:px-[0.75vw]">
-            <div>
-                <div className="flex items-center space-x-[1vw]">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="w-10 h-10"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        strokeWidth={2}
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M3 21v-4m0 0V5a2 2 0 012-2h6.5l1 1H21l-3 6 3 6h-8.5l-1-1H5a2 2 0 00-2 2zm9-13.5V9"
-                        />
-                    </svg>
-                    <h2 className="text-xl text-slate-700 dark:text-slate-200 lg:py-[2vh] lg:text-center lg:text-4xl">
-                        The Flag Game
-                    </h2>
-                </div>
+    <div className="space-y-[3vh]  lg:px-[0.75vw]">
+        <div>
+            <div className="flex items-center space-x-[1vw] ">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-10 h-10"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3 21v-4m0 0V5a2 2 0 012-2h6.5l1 1H21l-3 6 3 6h-8.5l-1-1H5a2 2 0 00-2 2zm9-13.5V9"
+                    />
+                </svg>
 
-                <h4 className="text-2xl font-normal leading-relaxed lg:leading-loose">
-                    <a
-                        href="https://github.com/JNat07/flag-game-frontend"
-                        className="font-normal underline decoration-blue-500 underline-offset-1"
-                    >
-                        The Flag Game
-                    </a>{" "}
-                    a simplistic yet fun way to test your knowledge. Able to
-                    play singleplayer and multiplayer to hones ones knowledge of
-                    countries flags.
-                </h4>
-
-                <ul className="text-2xl">
-                    <li>Multiplayer</li>
-                    <ul>
-                        <li>
-                            Receive and send real time game requests to others
-                            users in the lobby
-                        </li>
-                        <li>
-                            Choose a unique name to generate pixel art of your
-                            character
-                        </li>
-                    </ul>
-
-                    <li>Light && Dark mode</li>
-                </ul>
+                <h2 className=" text-xl text-slate-700 dark:text-slate-200 lg:py-[2vh] lg:text-4xl">
+                    <Typewriter
+                        words={["The Flag Game"]}
+                        cursor={true}
+                        cursorStyle={"_"}
+                    />
+                </h2>
             </div>
+
+            <h4 className="text-2xl font-normal leading-relaxed lg:leading-loose">
+                <a
+                    href="https://github.com/JNat07/flag-game-frontend"
+                    className="font-normal underline decoration-blue-500 underline-offset-1"
+                >
+                    The Flag Game
+                </a>{" "}
+                a simplistic yet fun way to test your knowledge. Able to play
+                singleplayer and multiplayer to hones ones knowledge of
+                countries flags.
+            </h4>
+
+            <ul className="text-2xl">
+                <li>Multiplayer</li>
+                <ul>
+                    <li>
+                        Receive and send real time game requests to others users
+                        in the lobby
+                    </li>
+                    <li>
+                        Choose a unique name to generate pixel art of your
+                        character
+                    </li>
+                </ul>
+
+                <li>Light && Dark mode</li>
+            </ul>
         </div>
     </div>
 );
