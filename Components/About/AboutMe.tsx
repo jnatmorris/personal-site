56031d3f60b4a337ba1fc758838e6dffad7b9820
Justin Morris <jnatmorris@gmail.com>
2022-06-07 17:25:33 -0400

    add about me

---
+++ Components/About/AboutMe.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+
+const AboutMe: React.FC = () => {
+    return (
+        <div className="col-span-2">
+            <div className="ml-[0.5vw] mr-[2vw] space-y-8 ">
+                <h2 className="text-3xl font-normal leading-relaxed dark:text-gray-200">
+                    I&apos;m originally from Chicago IL, and currently studying
+                    abroad a BSc. in Computer Science as well as perusing a BA.
+                    in Global Economics and Management, as my minor, at Jacobs
+                    University in Bremen Germany. During the summers I&apos;m
+                    based in The Big Apple &#x1F34E;{" "}
+                    <span className="text-xl text-slate-700/80 dark:text-slate-400/80">
+                        (NYC)
+                    </span>
+                    .
+                </h2>
+                <h2 className="text-3xl font-normal leading-relaxed dark:text-gray-200">
+                    If not studying, I spend my free time programming, traveling
+                    with family/friends, exercising, and working towards a
+                    private pilots license.
+                </h2>
+            </div>
+        </div>
+    );
+};
+
+export default AboutMe;
