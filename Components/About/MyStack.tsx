0dd2c0317963c50754e899fe3c22adfbadfd03b0
Justin Morris <jnatmorris@gmail.com>
2022-06-08 09:08:16 -0400

    update styling

--- Components/About/MyStack.tsx
+++ Components/About/MyStack.tsx
@@ -18,7 +18,7 @@ const MyStack: React.FC = () => (
                         d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
                     />
                 </svg>
-                <h3 className="text-xl font-medium text-slate-700 dark:text-slate-100/90 lg:py-[2vh] lg:text-center lg:text-4xl">
+                <h3 className="text-xl font-medium text-slate-700 dark:text-slate-100/90 lg:py-[2vh] lg:text-center lg:text-3xl">
                     My stack
                 </h3>
             </div>
