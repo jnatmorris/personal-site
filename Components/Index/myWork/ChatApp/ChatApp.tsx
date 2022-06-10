46b7b717e27999b5c3dfa7527099bce6d4decfaf
Justin Morris <jnatmorris@gmail.com>
2022-06-10 08:18:06 -0400

    update

--- Components/Index/myWork/ChatApp/ChatApp.tsx
+++ Components/Index/myWork/ChatApp/ChatApp.tsx
@@ -14,7 +14,7 @@ const ChatApp: React.FC = () => {
                         <path d="M2 5a2 2 0 012-2h7a2 2 0 012 2v4a2 2 0 01-2 2H9l-3 3v-3H4a2 2 0 01-2-2V5z" />
                         <path d="M15 7v2a4 4 0 01-4 4H9.828l-1.766 1.767c.28.149.599.233.938.233h2l3 3v-3h2a2 2 0 002-2V9a2 2 0 00-2-2h-1z" />
                     </svg>
-                    <h2 className="text-xl text-slate-700 dark:text-slate-200 lg:py-[2vh] lg:text-center lg:text-4xl">
+                    <h2 className="text-xl text-slate-700 dark:text-slate-200 lg:py-[2vh] lg:text-center lg:text-2xl">
                         <Typewriter
                             words={["The ChatApp"]}
                             cursor={true}
@@ -23,7 +23,7 @@ const ChatApp: React.FC = () => {
                     </h2>
                 </div>
 
-                <h3 className="text-2xl font-normal leading-relaxed lg:leading-loose">
+                <h3 className="text-xl font-normal leading-relaxed lg:leading-loose">
                     <a
                         href="https://github.com/JNat07/React-Socket.IO-ChatApp"
                         className="font-normal underline decoration-blue-500 underline-offset-2 "
@@ -43,7 +43,7 @@ const ChatApp: React.FC = () => {
                     devices. One day when I may come back to it.
                 </h3>
             </div>
-            <ul className="text-2xl">
+            <ul className="text-lg">
                 <li>Text communication</li>
                 <li>Accepts emojis</li>
                 <li>Joining and leaving messages</li>
