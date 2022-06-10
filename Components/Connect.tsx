f0e9a3e9064f08901a06f47582a8ca5c00a98dc7
Justin Morris <jnatmorris@gmail.com>
2022-06-10 13:49:58 -0400

    update style for mobile

--- Components/Connect.tsx
+++ Components/Connect.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Connect: React.FC = () => (
     <footer className="w-screen">
         <div className="mx-[2vw]  pb-1 lg:mx-[5vw] lg:flex lg:justify-end lg:pb-[2vh]">
-            <div className="flex justify-around space-x-[5vw] border-t-2 border-slate-300 px-[0.5vw] pt-1.5 prose-h3:m-0 dark:border-slate-600 lg:space-x-[3vw] ">
+            <div className="flex justify-around space-x-[5vw]  border-t border-slate-300 px-[0.5vw] pt-1.5 prose-h3:m-0 dark:border-slate-600 lg:space-x-[3vw] lg:border-t-2 ">
                 <h4>
                     <a
                         className="text-xl no-underline"
