1d27493338150749c2605e938e778d940ff714dd
Justin Morris <jnatmorris@gmail.com>
2022-06-10 08:11:14 -0400

    update styles

--- Components/About/MyStack.tsx
+++ Components/About/MyStack.tsx
@@ -18,18 +18,18 @@ const MyStack: React.FC = () => (
                         d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
                     />
                 </svg>
-                <h3 className="text-xl font-medium text-slate-700 dark:text-slate-100/90 lg:py-[2vh] lg:text-center lg:text-3xl">
+                <h3 className="text-xl font-medium text-slate-700 dark:text-slate-100/90 lg:py-[2vh] lg:text-center lg:text-2xl">
                     My stack
                 </h3>
             </div>
 
-            <h4 className="text-lg font-normal leading-relaxed dark:text-gray-200 lg:leading-loose">
+            <h4 className="font-normal leading-relaxed text-md dark:text-gray-200 lg:leading-loose">
                 While I have my personal preferences, I&apos;m always looking to
                 learn and experiment with new technologies. Believer that each
                 project has it&apos;s own set of tools.
             </h4>
 
-            <ul className="text-md mt-[2vh] list-outside dark:text-gray-200 lg:mt-[0vh]">
+            <ul className="mt-[2vh] list-outside text-sm dark:text-gray-200 lg:mt-[0vh]">
                 <li>
                     <h5>
                         ReactJS -{" "}
