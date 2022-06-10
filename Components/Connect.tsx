404de219c8fa099c0b0763d9e09d5c2b5fb3b72f
Justin Morris <jnatmorris@gmail.com>
2022-06-10 09:08:33 -0400

    updated padding

--- Components/Connect.tsx
+++ Components/Connect.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 const Connect: React.FC = () => (
-    <footer className="mt-[5vh] w-screen">
-        <div className="mx-[2vw] pb-1 lg:mx-[5vw] lg:flex lg:justify-end lg:pb-[2vh]">
+    <footer className="w-screen">
+        <div className="mx-[2vw] px-[2vw] pb-1 lg:mx-[5vw] lg:flex lg:justify-end lg:pb-[2vh]">
             <div className="mx-[0.5vw] flex justify-around space-x-[5vw] border-t-2 border-slate-300 pt-1.5 prose-h3:m-0 dark:border-slate-600 lg:space-x-[3vw] ">
                 <h4>
                     <a
