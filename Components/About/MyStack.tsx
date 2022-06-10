f59070c45327120b4cde032699e9fd88848ee2f0
Justin Morris <jnatmorris@gmail.com>
2022-06-10 12:51:06 -0400

    update font size

--- Components/About/MyStack.tsx
+++ Components/About/MyStack.tsx
@@ -18,7 +18,7 @@ const MyStack: React.FC = () => (
                         d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
                     />
                 </svg>
-                <h2 className="self-end text-xl font-normal leading-relaxed underline text-slate-700 underline-offset-2 dark:text-slate-100/90">
+                <h2 className="self-end text-2xl font-normal leading-relaxed underline text-slate-700 underline-offset-2 dark:text-slate-100/90">
                     My stack
                 </h2>
             </div>
