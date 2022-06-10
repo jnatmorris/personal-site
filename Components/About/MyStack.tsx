c9f1a0e49497514cfd0c4e970b3c09bf9dfb21c1
Justin Morris <jnatmorris@gmail.com>
2022-06-10 08:55:55 -0400

    text align bottom

--- Components/About/MyStack.tsx
+++ Components/About/MyStack.tsx
@@ -18,7 +18,7 @@ const MyStack: React.FC = () => (
                         d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
                     />
                 </svg>
-                <h2 className="text-xl font-normal leading-relaxed text-slate-700 dark:text-slate-100/90 lg:py-[2vh] lg:leading-loose">
+                <h2 className="align-bottom text-xl font-normal leading-relaxed text-slate-700 dark:text-slate-100/90 lg:py-[2vh] lg:leading-loose">
                     My stack
                 </h2>
             </div>
