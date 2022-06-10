a92bc0c9b682fb8ad962294cd7f5344209645b59
Justin Morris <jnatmorris@gmail.com>
2022-06-10 08:58:51 -0400

    update styling

--- Components/About/MyMentality.tsx
+++ Components/About/MyMentality.tsx
@@ -5,7 +5,7 @@ const MyMentality: React.FC = () => (
         <div className="flex items-center space-x-[1vw]">
             <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="w-10 h-10"
+                className="self-center w-10 h-10"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -17,7 +17,7 @@ const MyMentality: React.FC = () => (
                     d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
                 />
             </svg>
-            <h2 className="align-bottom text-xl font-normal leading-relaxed text-slate-700 underline underline-offset-2 dark:text-slate-100/90 lg:py-[2vh] lg:leading-loose">
+            <h2 className="self-end  align-bottom text-xl font-normal leading-relaxed text-slate-700 underline underline-offset-2 dark:text-slate-100/90 lg:py-[2vh] lg:leading-loose">
                 My mentality
             </h2>
         </div>
