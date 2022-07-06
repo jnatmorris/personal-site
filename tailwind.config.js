de91c4804b1b8e70f8657009d2e8977699ab813f
Justin Morris <jnatmorris@gmail.com>
2022-07-05 22:54:41 -0400

    update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,8 @@ module.exports = {
         "./pages/**/*.{js,ts,jsx,tsx}",
         "./Components/**/*.{js,ts,jsx,tsx}",
     ],
-    darkMode: "media",
-
     theme: {
         extend: {},
     },
-
     plugins: [require("@tailwindcss/typography")],
 };
