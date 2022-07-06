5307f9fef5dfc92a9a7ece4950e93f840dcac193
Justin Morris <jnatmorris@gmail.com>
2022-07-05 22:01:50 -0400

    test

--- Components/Layout.tsx
+++ Components/Layout.tsx
@@ -41,28 +41,6 @@ const Layout: React.FC<Props> = ({ children }) => {
     return (
         <LazyMotion strict features={domAnimation}>
             <MotionConfig reducedMotion="user">
-                <Head>
-                    <link
-                        rel="apple-touch-icon"
-                        sizes="180x180"
-                        href="/icons/apple-touch-icon.png"
-                    />
-                    <link
-                        rel="icon"
-                        type="image/png"
-                        sizes="32x32"
-                        href="/icons/favicon-32x32.png"
-                    />
-                    <link
-                        rel="icon"
-                        type="image/png"
-                        sizes="16x16"
-                        href="/icons/favicon-16x16.png"
-                    />
-
-                    <link rel="manifest" href="/site.webmanifest" />
-                </Head>
-
                 <div className="prose prose-h1:m-0 prose-h2:m-0 prose-h3:m-0 prose-h4:m-0 prose-p:m-0 prose-img:m-0 dark:prose-invert">
                     <main className="w-screen">
                         <Navigation />
