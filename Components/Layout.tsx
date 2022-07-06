5d6a3df1d5c589521e4415de64211ef09bcaa6c6
Justin Morris <jnatmorris@gmail.com>
2022-07-05 22:11:05 -0400

    update

--- Components/Layout.tsx
+++ Components/Layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from "next/head";
+import { LazyMotion, domAnimation } from "framer-motion";
 import Connect from "./Connect";
 import Navigation from "./Navigation";
 import ScrollTop from "./scrollTop";
@@ -9,36 +10,36 @@ interface Props {
 }
 
 const Layout: React.FC<Props> = ({ children }) => {
-    const [showUpArrow, setShowUpArrow] = React.useState<boolean>(false);
+    // const [showUpArrow, setShowUpArrow] = React.useState<boolean>(false);
 
-    React.useEffect(() => {
-        console.log("Welcome to my site! :)");
+    // React.useEffect(() => {
+    //     console.log("Welcome to my site! :)");
 
-        window.onscroll = function () {
-            // get scroll position of user
-            const viewportScrollDist =
-                window.pageYOffset ||
-                (
-                    document.documentElement ||
-                    document.body.parentNode ||
-                    document.body
-                ).scrollTop;
+    //     window.onscroll = function () {
+    //         // get scroll position of user
+    //         const viewportScrollDist =
+    //             window.pageYOffset ||
+    //             (
+    //                 document.documentElement ||
+    //                 document.body.parentNode ||
+    //                 document.body
+    //             ).scrollTop;
 
-            // if below first page or at bottom of page
-            if (
-                viewportScrollDist > window.innerHeight ||
-                window.innerHeight + window.scrollY >=
-                    document.body.offsetHeight
-            ) {
-                setShowUpArrow(true);
-            } else {
-                setShowUpArrow(false);
-            }
-        };
-    }, []);
+    //         // if below first page or at bottom of page
+    //         if (
+    //             viewportScrollDist > window.innerHeight ||
+    //             window.innerHeight + window.scrollY >=
+    //                 document.body.offsetHeight
+    //         ) {
+    //             setShowUpArrow(true);
+    //         } else {
+    //             setShowUpArrow(false);
+    //         }
+    //     };
+    // }, []);
 
     return (
-        <>
+        <LazyMotion strict features={domAnimation}>
             <Head>
                 <link
                     rel="apple-touch-icon"
@@ -68,10 +69,10 @@ const Layout: React.FC<Props> = ({ children }) => {
                     <div className="mt-[7vh]">
                         <Connect />
                     </div>
-                    <ScrollTop showUpArrow={showUpArrow} />
+                    {/* <ScrollTop showUpArrow={showUpArrow} /> */}
                 </main>
             </div>
-        </>
+        </LazyMotion>
     );
 };
 
