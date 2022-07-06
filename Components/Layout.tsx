9da76b6014a679b776a5c805b6f9f906aae440fe
Justin Morris <jnatmorris@gmail.com>
2022-07-05 22:58:54 -0400

    update

--- Components/Layout.tsx
+++ Components/Layout.tsx
@@ -72,7 +72,7 @@ const Layout: React.FC<Props> = ({ children }) => {
                 </Head>
 
                 <div className="prose prose-h1:m-0 prose-h2:m-0 prose-h3:m-0 prose-h4:m-0 prose-p:m-0 prose-img:m-0 dark:prose-invert">
-                    <main className="w-screen bg-white dark:bg-black">
+                    <main className="w-screen">
                         <Navigation />
                         {children}
                         <div className="mt-[7vh]">
