bc1f329e6060995a4f4c48f180c2336fd9ec7bbf
Justin Morris <jnatmorris@gmail.com>
2022-07-05 21:54:27 -0400

    test

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
     return (
-        <>
+        <Html lang="en">
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
@@ -16,6 +16,6 @@ export default function Document() {
                 <Main />
                 <NextScript />
             </body>
-        </>
+        </Html>
     );
 }
